6f79f0014574a895cfecb039903478af256d65b5
LlamaFan <nothingemailneu@gmail.com>
2024-12-18 16:15:20 +0100

    Add

---
+++ src/main/java/com/squid/Filter.java
@@ -0,0 +1,77 @@
+package com.squid;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Filter {
+    private int[][] imageArr;
+    private BufferedImage image;
+
+    private final int scale = 50;
+    private double ratX;
+    private double ratY;
+
+    private double w;
+    private double h;
+
+    private final String brightest = "@%#*+=-:. ";
+
+    public Filter() {
+        try {
+            image = ImageIO.read(new File("src/data/example3.jpg"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        initArr();
+
+        convertImageToArray(image);
+        printImage();
+    }
+
+    private void initArr() {
+        w = image.getWidth();
+        h = image.getHeight();
+
+        if (w >= h) {
+            ratX = scale;
+            ratY = scale * (h / w);
+        } else {
+            ratX = scale * (w / h);
+            ratY = scale;
+        }
+
+        imageArr = new int[(int) ratY][(int) ratX];
+    }
+
+    private void convertImageToArray(BufferedImage img) {
+        for (int i = 0; i < imageArr.length; i++)
+            for (int j = 0; j < imageArr[i].length; j++)
+                imageArr[i][j] = grayScale(
+                    img.getRGB((int) (w / ratX * j), (int) (h / ratY * i)));
+    }
+
+    private int grayScale(int pixel) {
+        int a = (pixel >> 24) & 0xff;
+        int r = (pixel >> 16) & 0xff;
+        int g = (pixel >> 8) & 0xff;
+        int b = pixel & 0xff;
+
+        return (r + g + b) / 3 * (a / 255);
+    }
+
+    private char numberToChar(int num) {
+        return brightest.charAt((int) (((double) num / 255) * (brightest.length() - 1)));
+    }
+
+    private void printImage() {
+        for (int i = 0; i < imageArr.length; i++) {
+            for (int j = 0; j < imageArr[i].length; j++)
+                System.out.print(numberToChar(imageArr[i][j]) + "");
+
+            System.out.println("");
+        }
+    }
+}
--- src/main/java/com/squid/Main.java
+++ src/main/java/com/squid/Main.java
@@ -1,17 +1,13 @@
-package com;
+package com.squid;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        new Filter();
+        /*int[][] arr = new int[20][10];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++)
+                System.out.print(arr[i][j]);
+            System.out.println("");
+        }*/
     }
 }
